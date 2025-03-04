29acd7cc3f67fb87e7d16a0282d899da123fe824
AndresD50s <a.dominguez@coadvil.edu.co>
2025-03-04 11:46:07 -0500

    Add files via upload

--- calculadoraconMenu.py
+++ calculadoraconMenu.py
@@ -1,10 +1,10 @@
-def suma(a+b):
+def suma(a, b):
     return a+b
 def resta(a, b):
     return a-b
-def multiplicacion(a, b)
+def multiplicacion(a, b):
     return a*b
-def division(a, b)
+def division(a, b):
     if b == 0:
         return "Error: division por cero"
     return a/b
@@ -27,20 +27,20 @@ def calculadora():
         if opcion in ["1", "2", "3", "4"]:
             num1 = float(input("Introduce el primer número"))
             num2  = float(input("Introduce el segundo número"))
-            if opcion == "1"
-            resultado = suma(num1, num2)
-            print(f"El resultado de {num1} + {num2} es: {resultado}")
-        elif opcion == "2"
-            resultado = resta(num1, num2)
-            print(f"El resultado de {num1} - {num2} es: {resultado}")
-        elif opcion == "3"
-            resultado = multiplicacion(num1, num2)
-            print(f"El resultado de {num1} * {num2} es: {resultado}")
-        elif opcion == "4"
-            resultado = division(num1, num2)
-            print(f"El resultado de {num1} / {num2} es: {resultado}")
-    else:
-        print("Opción no valida. Por favor, selecciona una opción del 1 al 5")
+            if opcion == "1":
+                resultado = suma(num1, num2)
+                print(f"El resultado de {num1} + {num2} es: {resultado}")
+            elif opcion == "2":
+                resultado = resta(num1, num2)
+                print(f"El resultado de {num1} - {num2} es: {resultado}")
+            elif opcion == "3":
+                resultado = multiplicacion(num1, num2)
+                print(f"El resultado de {num1} * {num2} es: {resultado}")
+            elif opcion == "4":
+                resultado = division(num1, num2)
+                print(f"El resultado de {num1} / {num2} es: {resultado}")
+        else:
+            print("Opción no valida. Por favor, selecciona una opción del 1 al 5")
 if __name__ == "__main__":
     calculadora()
 
